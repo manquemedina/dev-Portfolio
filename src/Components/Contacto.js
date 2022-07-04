c9f863562305e8833ea2373b939e88636e0b9c0c
Manque Medina <manquemedina1@gmail.com>
2022-07-04 11:45:54 -0300

    linkeado el contacto a db firestore, hay que armar formulario y cablear

--- src/Components/Contacto.js
+++ src/Components/Contacto.js
@@ -1,7 +1,38 @@
-import React from "react";
-
+import { async } from "@firebase/util";
+import { useState, useEffect } from "react";
+import { db } from "../firebase-config";
+import { collection, getDocs } from "firebase/firestore";
 const Contacto = (props) => {
-  return <></>;
+  const [name, setName] = useState("");
+  const [mail, setMail] = useState("");
+  const [message, setMessage] = useState("");
+  const nameRef = collection(db, "contacto");
+
+  useEffect(() => {
+    const getName = async () => {
+      const data = await getDocs(nameRef);
+      console.log(data.docs);
+    };
+    /*     const getMail = async (e) => {
+      e.target.value;
+    };
+    const getMessage = async (e) => {
+      e.target.value;
+    }; */
+    getName();
+    /*    getMail();
+    getMessage(); */
+  }, []);
+
+  return (
+    <>
+      <form type="submit">
+        <label placeholder="Nombre">
+          <input type={"string"} />
+        </label>
+      </form>
+    </>
+  );
 };
 
 export { Contacto };
